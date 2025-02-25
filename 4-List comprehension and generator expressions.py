a320e6d58d4ae7a956178dbd0a7c0c77468774c6
shahaz <ali.ghaderipour@gmail.com>
2025-02-25 21:44:38 +0330

    half of p5

--- 4-List comprehension and generator expressions.py
+++ 4-List comprehension and generator expressions.py
@@ -1,22 +1,26 @@
 xyz = []
+# generator => generates a stream in range of 0 to 5 | doesn't store in memory abit slower (less memory usage)
 for i in range(5):
     xyz.append(i)
 print("first  xyz {}".format(xyz))
-<<<<<<< HEAD
 
 
 
 
+# list comprehension => stores in memory and faster
 xyz = [i for i in range(5)]
 print("second xyz {}".format(xyz))
 
 
 
 # generator expressions
+# doesn't store in memory but it's a generator expression
 xyz = (i for i in range(5))
 for i in xyz:
     print(i)
-=======
+
+ 
 xyz = [i for i in range(5)]
 print("second xyz {}".format(xyz))
-# test
+
+

b4594d502e7cf32fe5c910a406cf366924c570ae
shahaz <ali.ghaderipour@gmail.com>
2025-03-01 21:18:33 +0330

    140312112118

--- 0-test_file.py
+++ 0-test_file.py
@@ -1,10 +1,3 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-class Person:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
-    
-    @property
-    def 

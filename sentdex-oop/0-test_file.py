43490e6fa87b52596f31c6fb6413e128b8fd2db4
Shahaz <ali.ghaderipour@gmail.com>
2025-03-05 13:24:36 +0330

    14031215 1324

--- sentdex-oop/0-test_file.py
+++ sentdex-oop/0-test_file.py
@@ -1,17 +1,3 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-def log_decorator(func):
-    def wrapper():
-        print(f"Calling function: {func.__name__}")
-        func()
-        print(f"Finished calling {func.__name__}")
-    return wrapper
-
-@log_decorator
-def say_hello():
-    """this function says hello."""
-    print("Hello, world!")
-
-print(say_hello.__name__)  # say_hello (not wrapper)
-print(say_hello.__doc__)   # This function says hello.
